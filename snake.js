fbfac65187aa230b8f6706362093dcb5bd3860c6
AbhiQuodes <bhartiabhinav618@gmail.com>
2024-06-27 23:36:41 +0530

    added inedx.html

--- snake.js
+++ snake.js
@@ -2,6 +2,8 @@ let inputDirection={ x:0, y:0 };
 let food={ x: Math.round( 1+(25-1)*Math.random()) ,  y: Math.round( 1+(21-1)*Math.random())};
 let snakeArray=[ {x:18 ,y:15}];
 let speed =5+snakeArray.length;
+let myScore=document.querySelector('#myScore');
+let hiScore=document.querySelector('#hiScore');
 let upBtn =document.querySelector('#slideUP');
 let downBtn =document.querySelector('#slideDown');
 let leftBtn =document.querySelector('#slideLeft');
@@ -33,8 +35,21 @@ let snakeHead,snakeFood;
 let startBtn = document.querySelector('#startBtn');
 let snakeZone=document.querySelector('#snakeZone');
 let startX,startY,DistX,DistY,touch;
-let i=0;
+let i=0,score=0,highScore;
 
+myScore.innerHTML="myScore : 0";
+if(localStorage.getItem('highScore')== null)
+    {
+        
+        localStorage.setItem('highScore','0');
+        highScore=0;
+
+    }
+
+    else{
+        highScore=localStorage.getItem('highScore');
+        hiScore.innerHTML="hiScore : "+highScore;
+    }
 
 function checkCollision(snakeArray){
 
@@ -49,9 +64,9 @@ function checkCollision(snakeArray){
         }
         if(window.innerWidth <=600)
             {
-                if( snakeArray[0].x< 0 ||   snakeArray[0].x > 17 ||snakeArray[0].y < 0 ||snakeArray[0].y > 22)
+                if( snakeArray[0].x<= 0 ||   snakeArray[0].x >= 17 ||snakeArray[0].y <= 0 ||snakeArray[0].y >= 22)
                     {
-                                        
+                                // console.log( "x = ",snakeArray[0].x,"y = ",snakeArray[0].y);         
                         return true;
                     }
     
@@ -62,9 +77,11 @@ function checkCollision(snakeArray){
             }
             else
             {
-                if( snakeArray[0].x< 0 ||   snakeArray[0].x > 26 ||snakeArray[0].y < 0 ||snakeArray[0].y > 22)
+                // console.log( "x = ",snakeArray[0].x,"y = ",snakeArray[0].y);       
+                if( snakeArray[0].x<=0 ||   snakeArray[0].x >= 26 ||snakeArray[0].y <= 0 ||snakeArray[0].y >= 22)
+           
                     {
-                                        
+                                      
                         return true;
                     }
     
@@ -77,131 +94,10 @@ function checkCollision(snakeArray){
 }
 
 
-// snake working function.
-
-function working(ctime){
-
-    window.requestAnimationFrame(working);
-   if((ctime-lastPaintTime)/1000<(1/speed))
-    {
-        return;
-    }
-
-   
-    lastPaintTime=ctime;
-    snakeZone.innerHTML=""; 
-    BackgroundMusic.play();
-   
-// forEach loop can be used here 
-for(i=0;i<snakeArray.length;i++)
-    {
-    
-       if(i==0)
-        {
-            snakeHead=document.createElement('div');
-            snakeHead.style.gridRowStart=snakeArray[i].y;
-            snakeHead.style.gridColumnStart=snakeArray[i].x;
-            snakeZone.appendChild(snakeHead);
-            snakeHead.classList.add('snakeHead');
-            
-        }
-        else{
-            snakeElement=document.createElement('div');
-            snakeElement.style.gridRowStart=snakeArray[i].y;
-            snakeElement.style.gridColumnStart=snakeArray[i].x;
-            snakeZone.appendChild(snakeElement);
-            snakeElement.classList.add('snakeBody');
-        }
-      
-
-    }
-
-
- 
-//motion of head of snake.
 
 
 
 
-       
-//motion of body of snake
-   
-    for(i=snakeArray.length-2;i>=0;i--)
-        {
-            snakeArray[i+1].x=snakeArray[i].x;
-            snakeArray[i+1].y=snakeArray[i].y;
-            // snakeArray[i+1]={...snakeArray[i]};
-          
-
-        }
-        snakeArray[0].x+=inputDirection.x;
-        snakeArray[0].y+=inputDirection.y;
-         // console.log(snakeArray[0].y); 
-    if(checkCollision(snakeArray))
-        {
-
-            if(window.innerWidth <=600)
-                {
-                    snakeArray=[ {x:10 ,y:15}];
-                    food={ x: Math.round( 1+(15-1)*Math.random()) ,  y: Math.round( 1+(20-1)*Math.random())};
-                }
-                else{
-                   
-                      snakeArray=[ {x:18 ,y:15}];
-                      food={ x: Math.round( 1+(24-1)*Math.random()) ,  y: Math.round( 1+(20-1)*Math.random())};
-                       
-                }
-       
-            inputDirection={x:0,y:0};
-            speed =5+snakeArray.length;
-            BackgroundMusic.pause();
-            gameOver.play();
-            alert("game over");
-           
-        }
-       
-
-      
-
-       
-
-
-// Eating the food  .....displaying the next food ....increase snake body size
-
-
-     
-
-    if(food.x== snakeArray[0].x && food.y==  snakeArray[0].y  )
-        {
-            // console.log("Eaten food yummy");
-            snakeArray.push({x:food.x ,y:food.y});
-            // console.log(snakeArray);
-            if( window.innerWidth<=600)
-                {
-                    food.x= Math.round( 1+(15-1)*Math.random()) ;
-                }
-                else{
-                    food.x= Math.round( 1+(24-1)*Math.random()) ;
-                }
-           
-            food.y= Math.round( 2+(20-2)*Math.random());
-            speed =5+(snakeArray.length)/2;
-            foodEat.play();
-            }
-
-
-            //display the food.
-            snakeFood=document.createElement('div');
-            snakeFood.style.gridRowStart= food.y;
-            snakeFood.style.gridColumnStart=food.x;
-            snakeZone.appendChild(snakeFood);
-            snakeFood.classList.add('food');
-
- 
-}
-
-
-//to start the game
 
 startBtn.addEventListener('click',(e)=>{
     
@@ -220,13 +116,14 @@ document.addEventListener('keydown',(e)=>{
 
 
 
-document.addEventListener('keyup',(event)=>{
+document.addEventListener('keydown',(event)=>{
     //  console.log(event.key);
      
     switch (event.key)
     {
         case 'ArrowUp':{
             inputDirection={ x:0, y:-1 };
+          ( lastpaintTime) =((lastPaintTime)/1000)-(1);
            DirectionChange.play();
             break;
         }
@@ -235,6 +132,7 @@ document.addEventListener('keyup',(event)=>{
 
         case 'ArrowDown':{
             inputDirection={ x:0, y:1 };
+            ( lastpaintTime) =((lastPaintTime)/1000)-(1);
             DirectionChange.play();
             break;
         }
@@ -242,6 +140,7 @@ document.addEventListener('keyup',(event)=>{
 
         case 'ArrowLeft':{
             inputDirection={ x:-1, y:0 };
+            ( lastpaintTime) =((lastPaintTime)/1000)-(1);
             DirectionChange.play();
         break;
         }
@@ -249,6 +148,7 @@ document.addEventListener('keyup',(event)=>{
         case 'ArrowRight':
             {
                 inputDirection={ x:1, y:0 };
+                ( lastpaintTime) =((lastPaintTime)/1000)-(1);
                 DirectionChange.play();
                 break;
             }
@@ -260,18 +160,21 @@ document.addEventListener('keyup',(event)=>{
 
 upBtn.addEventListener('click',()=>{
     inputDirection={ x:0, y:-1 };
+    ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
     DirectionChange.play();
 
 });
 
 downBtn.addEventListener('click',()=>{
     inputDirection={ x:0, y:1 };
+    ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
     DirectionChange.play();
 
 });
 
 leftBtn.addEventListener('click',()=>{
     inputDirection={ x:-1, y:0 };
+    ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
     DirectionChange.play();
 
 });
@@ -279,12 +182,13 @@ leftBtn.addEventListener('click',()=>{
 
 rightBtn.addEventListener('click',()=>{
     inputDirection={ x:1, y:0 };
+    ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
     DirectionChange.play();
 
 });
 
 
-document.addEventListener('touchstart',(e)=>{
+    snakeZone.addEventListener('touchstart',(e)=>{
     // alert("Touch start");
     touch=e.touches[0];
     startX=touch.clientX;
@@ -295,7 +199,7 @@ document.addEventListener('touchstart',(e)=>{
 })
 
 
-    document.addEventListener('touchmove',(e)=>{
+    snakeZone.addEventListener('touchmove',(e)=>{
     // alert("Touch Move");
     touch=e.touches[0];
     DistX=startX-touch.clientX;
@@ -306,7 +210,7 @@ document.addEventListener('touchstart',(e)=>{
 })
 
 
-    document.addEventListener('touchend',(e)=>{
+    snakeZone.addEventListener('touchend',(e)=>{
     // alert("Touch End");
   if(Math.abs(DistX)> Math.abs(DistY))
     {
@@ -314,6 +218,7 @@ document.addEventListener('touchstart',(e)=>{
             {
                 // console.log("drag Right");
                 inputDirection={ x:-1, y:0 };
+                ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
                 DirectionChange.play();
             }
 
@@ -321,6 +226,7 @@ document.addEventListener('touchstart',(e)=>{
                 {
                     // console.log("drag Left");
                     inputDirection={ x:1, y:0 };
+                    ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
                     DirectionChange.play();
                 }
 
@@ -332,6 +238,7 @@ document.addEventListener('touchstart',(e)=>{
             {
                 // console.log("drag Up");
                 inputDirection={ x:0, y:-1 };
+                ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
                 DirectionChange.play();
             }
 
@@ -339,6 +246,7 @@ document.addEventListener('touchstart',(e)=>{
                 {
                     // console.log("drag Down");
                     inputDirection={ x:0, y:1 };
+                    ( lastpaintTime) =((lastPaintTime)/1000)-(1/speed);
                     DirectionChange.play();
                 }
         
@@ -366,3 +274,189 @@ document.addEventListener('touchstart',(e)=>{
 
 
 // 
+
+// snake working function.
+
+function working(ctime){
+
+    window.requestAnimationFrame(working);
+   if((ctime-lastPaintTime)/1000<(1/speed))
+    {
+        return;
+    }
+
+   
+    lastPaintTime=ctime;
+   
+    snakeZone.innerHTML=""; 
+    hiScore.innerHTML="hiScore : "+highScore;
+    BackgroundMusic.play();
+   
+// forEach loop can be used here 
+// for(i=0;i<snakeArray.length;i++)
+//     {
+    
+//        if(i==0)
+//         {
+//             snakeHead=document.createElement('div');
+//             snakeHead.style.gridRowStart=snakeArray[i].y;
+//             snakeHead.style.gridColumnStart=snakeArray[i].x;
+//             snakeZone.appendChild(snakeHead);
+//             snakeHead.classList.add('snakeHead');
+            
+//         }
+//         else{
+//             snakeElement=document.createElement('div');
+//             snakeElement.style.gridRowStart=snakeArray[i].y;
+//             snakeElement.style.gridColumnStart=snakeArray[i].x;
+//             snakeZone.appendChild(snakeElement);
+//             snakeElement.classList.add('snakeBody');
+//         }
+      
+
+//     }
+
+      //display the food.
+      snakeFood=document.createElement('div');
+      snakeFood.style.gridRowStart= food.y;
+      snakeFood.style.gridColumnStart=food.x;
+      snakeZone.appendChild(snakeFood);
+      snakeFood.classList.add('food');
+
+
+ 
+//motion of head of snake.
+
+
+
+
+       
+//motion of body of snake
+   
+    // for(i=snakeArray.length-2;i>=0;i--)
+    //     {
+    //         snakeArray[i+1].x=snakeArray[i].x;
+    //         snakeArray[i+1].y=snakeArray[i].y;
+    //         // snakeArray[i+1]={...snakeArray[i]};
+          
+
+    //     }
+    //     snakeArray[0].x+=inputDirection.x;
+    //     snakeArray[0].y+=inputDirection.y;
+         // console.log(snakeArray[0].y); 
+
+
+
+
+
+
+         for(i=0;i<snakeArray.length;i++)
+            {
+            
+               if(i==0)
+                {
+                    snakeHead=document.createElement('div');
+                    snakeHead.style.gridRowStart=snakeArray[i].y;
+                    snakeHead.style.gridColumnStart=snakeArray[i].x;
+                    snakeZone.appendChild(snakeHead);
+                    snakeHead.classList.add('snakeHead');
+                    
+                }
+                else{
+                    snakeElement=document.createElement('div');
+                    snakeElement.style.gridRowStart=snakeArray[i].y;
+                    snakeElement.style.gridColumnStart=snakeArray[i].x;
+                    snakeZone.appendChild(snakeElement);
+                    snakeElement.classList.add('snakeBody');
+                }
+              
+        
+            }
+   
+      
+         
+         if(checkCollision(snakeArray))
+            {
+    
+                if(window.innerWidth <=600)
+                    {
+                        snakeArray=[ {x:10 ,y:15}];
+                        food={ x: Math.round( 1+(15-1)*Math.random()) ,  y: Math.round( 1+(20-1)*Math.random())};
+                    }
+                    else{
+                       
+                          snakeArray=[ {x:18 ,y:15}];
+                          food={ x: Math.round( 1+(24-1)*Math.random()) ,  y: Math.round( 1+(20-1)*Math.random())};
+                           
+                    }
+           
+                inputDirection={x:0,y:0};
+                speed =5+snakeArray.length;
+                BackgroundMusic.pause();
+                gameOver.play();
+                score=0;
+                localStorage.setItem('highScore',highScore);
+                myScore.innerHTML="myScore : 0";
+                alert("game over");
+               
+            }
+
+
+      
+
+       
+
+
+// Eating the food  .....displaying the next food ....increase snake body size
+
+
+     
+
+    if(food.x== snakeArray[0].x && food.y==  snakeArray[0].y  )
+        {
+            // console.log("Eaten food yummy");
+            snakeArray.push({x:food.x ,y:food.y});
+            // console.log(snakeArray);
+            if( window.innerWidth<=600)
+                {
+                    food.x= Math.round( 1+(15-1)*Math.random()) ;
+                }
+                else{
+                    food.x= Math.round( 1+(24-1)*Math.random()) ;
+                }
+           
+            food.y= Math.round( 2+(20-2)*Math.random());
+            speed =5+(snakeArray.length)/3;
+                score++;
+                myScore.innerHTML="myScore : "+score;
+                if(score >= highScore+1)
+                    {
+                        
+                        console.log("yes");
+                        highScore=score;
+                    }
+            foodEat.play();
+            }
+
+          
+
+           
+    
+                    for(i=snakeArray.length-2;i>=0;i--)
+                        {
+                            snakeArray[i+1].x=snakeArray[i].x;
+                            snakeArray[i+1].y=snakeArray[i].y;
+                            // snakeArray[i+1]={...snakeArray[i]};
+                          
+                
+                        }
+                        snakeArray[0].x+=inputDirection.x;
+                        snakeArray[0].y+=inputDirection.y;  
+
+                 
+
+
+        }
+
+
+//to start the game
